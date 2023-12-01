eced13b2ba14413cfdffa7b308cc69afa4e432c1
neivacoding <148003385+neivacoding@users.noreply.github.com>
2023-12-01 17:16:27 +0000

    Update Play_button.cs

--- Play_button.cs
+++ Play_button.cs
@@ -7,5 +7,6 @@ public class Play_button : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);    }
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);    
+    }
 }
